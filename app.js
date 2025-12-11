6b58d02ff1c1a6e072293259449257f440e2320b
김남우 <namwoo723@gmail.com>
2025-12-11 17:50:53 +0900

    Fix: app.js, fetch 제거 및 axios 추가

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteButton = document.getElementById('deleteMessage');
   const messageDisplay = document.getElementById('messageDisplay');
 
+  const BASE_URL = 'http://localhost:3000';
   // 서버로부터 메시지 가져오기
   fetchButton.addEventListener('click', async () => {
     try {
-      const response = await fetch('http://localhost:3000');
-      const data = await response.json();
+      const response = await axios.get(BASE_URL);
+      const data = response.data;
       messageDisplay.textContent = data.message || '메시지가 없습니다';
     } catch (error) {
       console.error('메시지 가져오기 오류:', error);
@@ -19,32 +20,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 서버에 메시지 업데이트 요청 보내기
   updateButton.addEventListener('click', async () => {
-    const newMessage = prompt('새로운 메시지를 입력하세요:');
-    if (newMessage) {
-      try {
-        const response = await fetch('http://localhost:3000', {
-          method: 'PUT',
+    try {
+      const newMessage = prompt('새로운 메시지를 입력하세요:');
+      if (!newMessage) return;
+        const response = await axios.put(BASE_URL, newMessage, {
           headers: {
             'Content-Type': 'text/plain',
           },
-          body: newMessage,
         });
-        const data = await response.text();
-        messageDisplay.textContent = data;
-      } catch (error) {
-        console.error('메시지 업데이트 오류:', error);
-      }
+        messageDisplay.textContent = response.data;
+    } catch (error) {
+      console.error('메시지 업데이트 오류:', error);
     }
   });
 
   // 서버에 메시지 삭제 요청 보내기
   deleteButton.addEventListener('click', async () => {
     try {
-      const response = await fetch('http://localhost:3000', {
-        method: 'DELETE',
-      });
-      const data = await response.text();
-      messageDisplay.textContent = data;
+      const response = await axios.delete(BASE_URL);
+      messageDisplay.textContent = response.data;
     } catch (error) {
       console.error('메시지 삭제 오류:', error);
     }
